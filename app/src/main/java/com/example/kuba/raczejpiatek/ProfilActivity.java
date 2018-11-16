670ea12f5578162210dc21940020580fc44c2c6c
Daymos1996 <38887719+Daymos1996@users.noreply.github.com>
2018-11-16 16:59:46 +0100

    Usuwanie uzytownika

--- app/src/main/java/com/example/kuba/raczejpiatek/ProfilActivity.java
+++ app/src/main/java/com/example/kuba/raczejpiatek/ProfilActivity.java
@@ -1,17 +1,23 @@
 package com.example.kuba.raczejpiatek;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -26,10 +32,14 @@ import com.example.kuba.raczejpiatek.user.User;
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -59,8 +69,10 @@ public class ProfilActivity extends AppCompatActivity {
     private TextView nickNameTextView;
     private TextView phoneNumberTextView;
     private Button goToMapBtn;
+    private Button deleteUser;
     private Button goToFindFriendsBtn;
     private String userID;
+
 
 
     @Override
@@ -72,7 +84,7 @@ public class ProfilActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         myRef=database.getReference();
-        FirebaseUser user=mAuth.getCurrentUser();
+        final FirebaseUser user=mAuth.getCurrentUser();
         userID=user.getUid();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -118,6 +130,16 @@ public class ProfilActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
+
+             deleteUser.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View view) {
+                     deleteUser(userID);
+                     Intent intent = new Intent(ProfilActivity.this,LoginActivity.class);
+                     startActivity(intent);
+                 }
+             });
+
 
     }
 
@@ -185,6 +207,149 @@ public class ProfilActivity extends AppCompatActivity {
     }
 
 
+    public void updateProfile() {
+        // [START update_profile]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName("Jane Q. User")
+                .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User profile updated.");
+                        }
+                    }
+                });
+        // [END update_profile]
+    }
+
+    public void updateEmail() {
+        // [START update_email]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.updateEmail("user@example.com")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User email address updated.");
+                        }
+                    }
+                });
+        // [END update_email]
+    }
+
+
+    public void updatePassword() {
+        // [START update_password]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String newPassword = "SOME-SECURE-PASSWORD";
+
+        user.updatePassword(newPassword)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User password updated.");
+                        }
+                    }
+                });
+        // [END update_password]
+    }
+
+    public void deleteUser(String userID) {
+
+       FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userData = FirebaseDatabase.getInstance().getReference("Users").child(userID);
+
+        userData.removeValue()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "User account deleted.");
+                }
+            }
+        });
+
+
+       user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User account deleted.");
+                        }
+                    }
+                });
+
+    }
+
+    public void reauthenticate() {
+        // [START reauthenticate]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Get auth credentials from the user for re-authentication. The example below shows
+        // email and password credentials but there are multiple possible providers,
+        // such as GoogleAuthProvider or FacebookAuthProvider.
+        AuthCredential credential = EmailAuthProvider
+                .getCredential("jakub@jakub.pl", "jakub1");
+
+        // Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d(TAG, "User re-authenticated.");
+                    }
+                });
+        // [END reauthenticate]
+    }
+
+    private boolean updateArtist(String id, String name) {
+        //getting the specified artist reference
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(userID);
+
+        //updating artist
+        User user = new User();
+        user.setFirst_name(name);
+        Toast.makeText(getApplicationContext(), "Artist Updated", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private void showUpdateDeleteDialog(final String userID, String First_name) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.update_data, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+
+        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateArtist);
+
+        dialogBuilder.setTitle(First_name);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+                if (!TextUtils.isEmpty(name)) {
+                    updateArtist(userID, name);
+                    b.dismiss();
+                }
+            }
+        });
+    }
+
 
 
 
@@ -198,7 +363,7 @@ public class ProfilActivity extends AppCompatActivity {
         profilURL=findViewById(R.id.avatar);
         goToMapBtn = (Button) findViewById(R.id.go_to_map_btn);
         goToFindFriendsBtn = findViewById(R.id.go_to_find_friends_btn);
-
+        deleteUser =(Button) findViewById(R.id.deleteUser);
     }
     private void toastMessage(String message){
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
